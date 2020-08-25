65eae616760784f6f71371545d72cd362bf3ea45
SergSmith64 <sergsmith2008@yandex.ru>
2020-08-25 11:55:29 +0400

    fix: Header

--- ss64/src/Components/Header.js
+++ ss64/src/Components/Header.js
@@ -10,58 +10,58 @@ import Blog from '../Pages/Blog';
 
 
 export default class Header extends Component {
-    render() {
-        return (
-            <>
-                <Router>
-                    {/* <Navbar fixed="top" collapseOnSelect expand="md" bg="dark" variant="dark" > */}
-                    <Navbar collapseOnSelect expand="md" bg="dark" variant="dark" >
-                        <Container>
-                            <Navbar.Brand href="/">
-                                <img
-                                    src={logo}
-                                    height="30"
-                                    width="30"
-                                    className="d-inline-block align-top"
-                                    alt="Logo"
-                                />{" "}
+	render() {
+		return (
+			<>
+				<Router>
+					{/* <Navbar fixed="top" collapseOnSelect expand="md" bg="dark" variant="dark" > */}
+					<Navbar collapseOnSelect expand="md" bg="dark" variant="dark" >
+						<Container>
+							<Navbar.Brand href="/">
+								<img
+									src={logo}
+									height="30"
+									width="30"
+									className="d-inline-block align-top"
+									alt="Logo"
+								/>{" "}
 						React
 					</Navbar.Brand>
-                            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                            <Navbar.Collapse id="responsive-navbar-nav">
-                                <Nav className="mr-auto">
-                                    {/* <Nav.Link href="/"> Home </Nav.Link> */}
-                                    {/* <Nav.Link href="/about"> About us </Nav.Link> */}
-                                    {/* <Nav.Link href="/contacts"> Contacts </Nav.Link> */}
-                                    {/* <Nav.Link href="/blog"> Blog </Nav.Link> */}
+							<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+							<Navbar.Collapse id="responsive-navbar-nav">
+								<Nav className="mr-auto">
+									{/* <Nav.Link href="/"> Home </Nav.Link> */}
+									{/* <Nav.Link href="/about"> About us </Nav.Link> */}
+									{/* <Nav.Link href="/contacts"> Contacts </Nav.Link> */}
+									{/* <Nav.Link href="/blog"> Blog </Nav.Link> */}
 
-                                    <Nav.Link as={Link} to="/">Home</Nav.Link>
-                                    <Nav.Link as={Link} to="/about"> About us </Nav.Link>
-                                    <Nav.Link as={Link} to="/contacts"> Contacts </Nav.Link>
-                                    <Nav.Link as={Link} to="/blog"> Blog </Nav.Link>
-                                </Nav>
-                                <Form inline >
-                                    <FormControl
-                                        type="text"
-                                        placeholder="Search"
-                                        className="mr-sm-2"
-                                    />
-                                    <Button variant="outline-info">Search</Button>
-                                </Form>
-                            </Navbar.Collapse>
-                        </Container>
-                    </Navbar>
+									<Nav.Link as={Link} to="/">Главная</Nav.Link>
+									<Nav.Link as={Link} to="/about">Сайты</Nav.Link>
+									<Nav.Link as={Link} to="/contacts">Контакты</Nav.Link>
+									<Nav.Link as={Link} to="/blog">Блог</Nav.Link>
+								</Nav>
+								<Form inline >
+									<FormControl
+										type="text"
+										placeholder="что-то ищем?"
+										className="mr-sm-2"
+									/>
+									<Button variant="outline-info">Поиск</Button>
+								</Form>
+							</Navbar.Collapse>
+						</Container>
+					</Navbar>
 
 
-                    {/* -- open-tag <Router> remove below return -- */}
-                    <Switch>
-                        <Route exact path="/" component={Home} />
-                        <Route exact path="/about" component={About} />
-                        <Route exact path="/contacts" component={Contacts} />
-                        <Route exact path="/blog" component={Blog} />
-                    </Switch>
-                </Router>
-            </>
-        )
-    }
+					{/* -- open-tag <Router> remove below return -- */}
+					<Switch>
+						<Route exact path="/" component={Home} />
+						<Route exact path="/about" component={About} />
+						<Route exact path="/contacts" component={Contacts} />
+						<Route exact path="/blog" component={Blog} />
+					</Switch>
+				</Router>
+			</>
+		)
+	}
 }
