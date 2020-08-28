8dc20ff2b165d19a0f75a1285444372eec6ecf34
SergSmith64 <sergsmith2008@yandex.ru>
2020-08-28 13:55:57 +0400

    feat: replace css to scss

--- ss64/src/App.js
+++ ss64/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 // import { Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import Header from './Components/Header';
 
 function App() {
--- ss64/src/Pages/About.js
+++ ss64/src/Pages/About.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Container, Tab, Nav, Row, Col } from 'react-bootstrap'
-import './about.scss';
+import './About.scss';
 
 export default class About extends Component {
 	render() {
