13471aaaa6137e6d8542421612e0e06d578d29fe
SergSmith64 <sergsmith2008@yandex.ru>
2021-03-06 14:04:32 +0400

    31: Fix: Ads

---
+++ ss64/build/precache-manifest.b9fb3fae1ac8d38b5db85862db48d774.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6b3cdf1f758e50112666c91514e3e8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65dd5f8dce1288f258ea",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "f23c6784f13504363150",
+    "url": "/static/css/main.ae0ad23b.chunk.css"
+  },
+  {
+    "revision": "65dd5f8dce1288f258ea",
+    "url": "/static/js/2.e57df6ad.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e57df6ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f23c6784f13504363150",
+    "url": "/static/js/main.4d389a90.chunk.js"
+  },
+  {
+    "revision": "24e2c80827ff06131c62",
+    "url": "/static/js/runtime-main.cfc0680d.js"
+  },
+  {
+    "revision": "bb41aa26b401dc2a79b8164b9e8cf61e",
+    "url": "/static/media/creating-adaptive-sites.bb41aa26.jpg"
+  },
+  {
+    "revision": "3d2ac192e2019000a2682af1c49d2579",
+    "url": "/static/media/sergsmith64-avtoservice.3d2ac192.jpg"
+  },
+  {
+    "revision": "da6c674d6f4fefbed3743524bfb9bc03",
+    "url": "/static/media/sergsmith64-banbas64.da6c674d.jpg"
+  },
+  {
+    "revision": "49233f231a46ebbc451b59512b65223d",
+    "url": "/static/media/sergsmith64-betonnij-zavod.49233f23.jpg"
+  },
+  {
+    "revision": "932ba40f616dfdd941640c1db3d65d1d",
+    "url": "/static/media/sergsmith64-germany-life.932ba40f.jpg"
+  },
+  {
+    "revision": "bea1dab8f9b8b86b225bb398a62a0793",
+    "url": "/static/media/sergsmith64-inoutlab.bea1dab8.jpg"
+  },
+  {
+    "revision": "2e9c6c39c04f544afbb6bbbab9a0307f",
+    "url": "/static/media/sergsmith64-isramedmoscow.2e9c6c39.jpg"
+  },
+  {
+    "revision": "dc36c29ec639309f3a9256e415d670c5",
+    "url": "/static/media/sergsmith64-polytehservice.dc36c29e.jpg"
+  },
+  {
+    "revision": "f55cba48e617cc199fc2035ae4d53875",
+    "url": "/static/media/sergsmith64-singulf.f55cba48.jpg"
+  },
+  {
+    "revision": "5eb453a3349ba79d8a520a5c8a3f65a6",
+    "url": "/static/media/sergsmith64-skupka-radioloma-drm64.5eb453a3.jpg"
+  },
+  {
+    "revision": "129d949dd2effcb091364f1537b804b6",
+    "url": "/static/media/sergsmith64-spec-agro-trans.129d949d.jpg"
+  },
+  {
+    "revision": "77cd347dd0989475d1c9b363a9faf6cf",
+    "url": "/static/media/sergsmith64-specii-i-pryanosti.77cd347d.jpg"
+  },
+  {
+    "revision": "a4840d44e67395fd0ae7415b52c75fdd",
+    "url": "/static/media/sergsmith64-svoidom-info.a4840d44.jpg"
+  },
+  {
+    "revision": "b7022030fa346cab27e93df5b30ba7c4",
+    "url": "/static/media/sozdanie-adaptivnyh-saitov.b7022030.jpg"
+  },
+  {
+    "revision": "1cf7ed6eabc1ff8f7183bee2962d5c7f",
+    "url": "/static/media/ss64.1cf7ed6e.png"
+  }
+]);
